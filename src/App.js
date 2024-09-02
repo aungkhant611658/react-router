75af67a8616e0612e579d1c48d2386f19f515c6f
aungkhant611658 <aungkhant611658@gmail.com>
2024-09-02 21:25:03 +0630

    set default route

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
       {
-        path: "/",
+        index: true,
         element: <Home />,
       },
       {
