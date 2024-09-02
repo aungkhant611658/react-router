5c5221b29ed946bfa7eb4d24dd86c91cf51f0c72
aungkhant611658 <aungkhant611658@gmail.com>
2024-09-02 17:34:27 +0630

    relative route

---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,19 @@
+import { Link, useParams } from "react-router-dom";
+
+function ProductDetail() {
+  const params = useParams();
+
+  return (
+    <>
+      <h1>ProductDetail</h1>
+      <p>{params.productId}</p>
+      <p>
+        <Link to=".." relative="path">
+          Back
+        </Link>
+      </p>
+    </>
+  );
+}
+
+export default ProductDetail;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,5 +1,33 @@
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+  {
+    id: "p1",
+    title: "Product 1",
+  },
+  {
+    id: "p2",
+    title: "Product 2",
+  },
+  {
+    id: "p3",
+    title: "Product 3",
+  },
+];
+
 function Products() {
-  return <div>My Products Page</div>;
+  return (
+    <>
+      <div>My Products Page</div>
+      <ul>
+        {PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default Products;
